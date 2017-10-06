7021aec88f3da15ce1166b42e60c4ecdea14e24c
XiaoXOFLaptop <xiaoxiongyouxiang0@163.com>
2017-10-06 21:13:20 +0800

    修改了一些信息

--- src/com/xiaox/demo003/Solution01.java
+++ src/com/xiaox/demo003/Solution01.java
@@ -1,8 +1,11 @@
 package com.xiaox.demo003;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by XiaoX on 2017/9/26.
  * 找到不包含重复元素的最长字串的长度
+ * 在这小小的佩服下自己，竟然想到用队列，呀哈哈哈！
  */
 public class Solution01 {
     public int lengthOfLongestSubstring(String s) {
@@ -11,31 +14,28 @@ public class Solution01 {
             return 0;
         }
         int max = 1;
-        int start = 0;
-        StringBuffer str = new StringBuffer();
-        for (int j = start + max - 1; j < chars.length; j++) {
-            if (!str.toString().contains(chars[j] + "")) {
-                str.append(chars[j]);
+        Queue<String> queue = new LinkedList<String>();
+        for (int j = 0; j < chars.length; j++) {
+            if (!queue.contains(chars[j]+"")) {
+                queue.add(chars[j]+"");
             } else {
-                start++;
-                if (str.length() > max) {
-                    max = str.length();
-                    str = new StringBuffer();
+                if (queue.size() > max) {
+                    max = queue.size();
                 }
+                while (queue.size()>0 && !queue.poll().equals(chars[j]+""));
+                queue.add(chars[j]+"");
             }
         }
-        if (str.length() > max) max = str.length();
+        if (queue.size() > max) max = queue.size();
         return max;
     }
 
     public static void main(String[] args) {
-        String[] testCases = new String[]{"", "dvdf", "abcabcbb", "bbbbb", "pwwkew", "c"};
+        String[] testCases = new String[]{"", "dvdf", "abcabcbb", "bbbbb", "pwwkew", "c", "aab"};
         int i = 0;
         for (String testCase : testCases) {
             System.out.print("测试");
             System.out.println(++i + "--->" + new Solution01().lengthOfLongestSubstring(testCase));
         }
-
     }
-
 }
